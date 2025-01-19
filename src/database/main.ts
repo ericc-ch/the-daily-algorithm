a79e806f8a3428473713f23216b6a010cdc0b914
Erick Christian <erickchristian48@gmail.com>
2025-01-19 19:14:01 +0700

    feat: Add @libsql/client dependency and update database connection method

--- src/database/main.ts
+++ src/database/main.ts
@@ -1,8 +1,9 @@
-import { Database } from "bun:sqlite"
-import { drizzle } from "drizzle-orm/bun-sqlite"
+import { drizzle } from "drizzle-orm/libsql"
 
 import { PATHS } from "~/lib/paths"
 
-const sqlite = new Database(PATHS.DB_PATH)
-
-export const db = drizzle(sqlite)
+export const db = drizzle({
+  connection: {
+    url: `file:${PATHS.DB_PATH}`,
+  },
+})
