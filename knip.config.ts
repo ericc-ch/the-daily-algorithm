ac79bc1701e490ba12433e107b6b8a22466f0790
Erick Christian <erickchristian48@gmail.com>
2025-02-01 00:41:11 +0700

    chore: Update dependencies and add new entry points in configuration

--- knip.config.ts
+++ knip.config.ts
@@ -1,5 +1,9 @@
 import type { KnipConfig } from "knip"
 
 export default {
-  entry: ["./src/main.ts"],
+  entry: [
+    "./src/main.ts",
+    "./src/modules/video-renderer/remotion-entry.ts",
+    "./remotion.config.ts",
+  ],
 } satisfies KnipConfig
